4fc5582200fb37bc6f4bc6001df20ac045a3e6ad
danny <dannysht27@gmail.com>
2024-11-23 21:52:03 +0200

    added dom tasks

---
+++ DOM-tasks/table/script.js
@@ -0,0 +1,7 @@
+const tableData = [
+    { id: 1, name: 'Пешо', age: 25, country: 'България', profession: 'Програмист' },
+    { id: 2, name: 'Ханс', age: 30, country: 'Германия', profession: 'Автомонтьор' },
+    { id: 3, name: 'Хенриете', age: 35, country: 'Дания', profession: 'HR' },
+    { id: 4, name: 'Алис', age: 28, country: 'Франция', profession: 'Учител' },
+    { id: 5, name: 'Джефри', age: 40, country: 'САЩ', profession: 'Рапър' }
+];
